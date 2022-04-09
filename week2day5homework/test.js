00ef5e42a29974fb0582e2e73ad8e0073b670186
Martia <pureromancebymartia@gmail.com>
2022-04-09 19:31:48 -0400

    welcome

---
+++ week2day5homework/test.js
@@ -0,0 +1,117 @@
+alert("Hello, World!"); 
+
+// the second alert
+alert("Hello, World");
+var myVariableMartia;
+undefined
+myVariableMartia=10;
+10
+var myVar2="hello";
+undefined
+myVar2
+'hello'
+myVar2="hello again";
+'hello again'
+myVar2myVar2=5;
+5
+myVar2=5;
+5
+var customerAge=32;
+undefined
+customerAge;
+32
+var myVar3=40;
+undefined
+int myVar3=40;
+VM430:1 Uncaught SyntaxError: Unexpected identifier
+var myVar=5;
+undefined
+myVar
+5
+5+5
+10
+myVar+10=
+VM525:1 Uncaught SyntaxError: Invalid left-hand side in assignment
+myVar +10
+15
+myVar
+5
+myVar =myVarm+10
+VM659:1 Uncaught ReferenceError: myVarm is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM659:1
+myVar = myVar =10
+10
+myVar = myVar+5
+15
+myVar
+15
+10-5
+5
+myVar -3
+12
+myVar = myVar - 5
+10
+10 * 10
+100
+20/20
+1
+myVar = myVar/2
+5
+myVar
+5
+5+ "hello"
+'5hello'
+5= "hello"
+VM1059:1 Uncaught SyntaxError: Invalid left-hand side in assignment
+"5hello"
+'5hello'
+100 + "hello"
+'100hello'
+"hello" + "david"
+'hellodavid'
+"hello" + " david"
+'hello david'
+5 * "hello"
+NaN
+var myVar=10
+undefined
+myVar = myVar + 5
+15
+myVar
+15
+myVar += 5
+20
+myVarmyVar -= 10
+VM1433:1 Uncaught ReferenceError: myVarmyVar is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM1433:1
+myVar=10
+    
+10
+myVar /= 2
+5
+myVar*= 5
+25
+myVar += 1
+26
+myVar++
+26
+myVar
+27
+++myVar
+28
+var myVar = "Martia"
+undefined
+document.write(myVar)
+Navigated to http://127.0.0.1:5500/week2day5homework/index.html
+undefined
+console.log(myVar)
+VM1824:1 Martia
+undefined
+document.write(10)
+undefined
+document.write(20/5)
+undefined
+
+
