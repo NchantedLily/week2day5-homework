462acec96bd4f1980f452e80e739cedce47a6864
Martia <pureromancebymartia@gmail.com>
2022-04-09 20:08:37 -0400

    welcome

---
+++ test.js
@@ -0,0 +1,114 @@
+alert("Hello, World!"); 
+
+// the second alert
+alert("Hello, World");
+var myVariableMartia;
+undefined
+myVariableMartia=10;
+10
+var myVar2="hello";
+undefined
+myVar2
+'hello'
+myVar2="hello again";
+'hello again'
+myVar2myVar2=5;
+5
+myVar2=5;
+5
+var customerAge=32;
+undefined
+customerAge;
+32
+var myVar3=40;
+undefined
+ myVar3=40;
+
+var myVar=5;
+undefined
+myVar
+5
+5+5
+10
+myVar+10
+
+myVar +10
+15
+myVar
+5
+myVar =myVarm+10
+
+(anonymous) 
+myVar = myVar =10
+10
+myVar = myVar+5
+15
+myVar
+15
+10-5
+5
+myVar -3
+12
+myVar = myVar - 5
+10
+10 * 10
+100
+20/20
+1
+myVar = myVar/2
+5
+myVar
+5
+5+ "hello"
+'5hello'
+5= "hello"
+"5hello"
+'5hello'
+100 + "hello"
+'100hello'
+"hello" + "david"
+'hellodavid'
+"hello" + " david"
+'hello david'
+5 * "hello"
+NaN
+var myVar=10
+undefined
+myVar = myVar + 5
+15
+myVar
+15
+myVar += 5
+20
+myVarmyVar -= 10
+
+  
+myVar=10
+    
+10
+myVar /= 2
+5
+myVar*= 5
+25
+myVar += 1
+26
+myVar++
+26
+myVar
+27
+++myVar
+28
+var myVar = "Martia"
+undefined
+document.write(myVar)
+
+undefined
+console.log(myVar)
+VM1824:1 
+undefined
+document.write(10)
+undefined
+document.write(20/5)
+undefined
+
+
